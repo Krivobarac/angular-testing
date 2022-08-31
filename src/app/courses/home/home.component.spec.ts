34025fbfd654d437ada30a27a038736ad54be85b
Krivobarac <gasparovicsasa@yahoo.com>
2022-08-31 23:39:21 +0200

    component testing, last one assertion uncompleted

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,26 +1,27 @@
-import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, waitForAsync} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesModule} from '../courses.module';
 import {DebugElement} from '@angular/core';
 
 import {HomeComponent} from './home.component';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {CoursesService} from '../services/courses.service';
-import {HttpClient} from '@angular/common/http';
-import {COURSES} from '../../../../server/db-data';
 import {setupCourses} from '../common/setup-test-data';
 import {By} from '@angular/platform-browser';
 import {of} from 'rxjs';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {click} from '../common/test-utils';
-import { AppModule } from '../../app.module';
+import SpyObj = jasmine.SpyObj;
 
 describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let component: HomeComponent;
   let el: DebugElement;
+  let coursesServiceSpy: SpyObj<CoursesService>;
+
+  const filteredCourses = (type: 'BEGINNER' | 'ADVANCED') => setupCourses().filter(course => course.category === type);
 
   beforeEach(waitForAsync( () => {
-    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
+    coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
 
     TestBed.configureTestingModule({
       imports: [
@@ -29,15 +30,14 @@ describe('HomeComponent', () => {
         HttpClientTestingModule
       ],
       providers: [
-        {provide: CoursesService, value: coursesServiceSpy}
+        {provide: CoursesService, useValue: coursesServiceSpy}
       ]
-    })
-    .compileComponents()
-    .then(() => {
-      fixture = TestBed.createComponent(HomeComponent);
-      component = fixture.componentInstance;
-      el = fixture.debugElement;
-    });
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
   }) );
 
   it("should create the component", () => {
@@ -46,22 +46,45 @@ describe('HomeComponent', () => {
 
 
   it("should display only beginner courses", () => {
-    pending();
+    coursesServiceSpy.findAllCourses.and.returnValue(of(filteredCourses('BEGINNER')));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
   });
 
 
   it("should display only advanced courses", () => {
-      pending();
+    coursesServiceSpy.findAllCourses.and.returnValue(of(filteredCourses('ADVANCED')));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
   });
 
 
   it("should display both tabs", () => {
-    pending();
+    coursesServiceSpy.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    expect(tabs.length).toBe(2, 'Unexpected number of tabs found');
   });
 
 
   it("should display advanced courses when tab clicked", () => {
-    pending();
+    coursesServiceSpy.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    console.log(tabs)
+
+    click(tabs[1]);
+    fixture.detectChanges(); // component doesn't see second tab's card list but see first of
+
+    const cardTitles = el.queryAll(By.css('.mat-card-title'));
+    expect(cardTitles.length).toBeGreaterThan(0, 'Could not find card titles');
+    expect(cardTitles[0].nativeElement.textContent).toBe(filteredCourses('ADVANCED')[0].titles.description);
   });
 
 });
